4bf3bdaa9cf90008d7c375bb464855469939c11f
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-03-08 12:05:44 -0400

    Added create sprite button

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,19 +1,27 @@
 extends Control
 
 
-@onready var sprite_tree_reference := $'HSplitContainer/SpriteTree'
+@onready var sprite_tree_reference := $'HSplitContainer/PanelContainer/MarginContainer/VBoxContainer/SpriteTree'
 
-@onready var sprite_icon := preload('res://icons/sprite.svg')
+@onready var sprite_icon_image := preload('res://icons/sprite.svg')
+
+@onready var root: TreeItem = sprite_tree_reference.create_item()
 
 
 func _ready() -> void:
-	var root: TreeItem = sprite_tree_reference.create_item()
-	root.set_text(0, 'Root')
-
-	for index in 10:
-		var new_sprite: TreeItem = sprite_tree_reference.create_item(root)
-		new_sprite.set_text(0, str(randi()))
-		new_sprite.set_icon(0, sprite_icon)
+	pass
 
 func _process(delta: float) -> void:
 	pass
+
+
+func create_sprite(sprite_name: String, sprite_icon=sprite_icon_image) -> TreeItem:
+	var new_sprite: TreeItem = sprite_tree_reference.create_item(root)
+
+	new_sprite.set_text(0, sprite_name)
+	new_sprite.set_icon(0, sprite_icon)
+
+	return new_sprite
+
+func _on_create_sprite_button_pressed() -> void:
+	create_sprite('New Sprite')
