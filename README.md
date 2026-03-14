8c6a5f8b79791ea94ab277c8ed186e091aa26d0a
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-03-14 19:28:02 -0400

    Updated credit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Fleet Animator
 ## Attribution
-- [**curve_editor.gd**](https://github.com/JtotheThree/godot_curve_editor/blob/main/curve_editor.gd) Copyright (c) 2024 JtotheThree, licensed under [MIT license](https://github.com/JtotheThree/godot_curve_editor/blob/main/LICENSE)
+- [curve_editor.gd](scripts/curve_editor.gd) - Adapted from [godot_curve_editor](https://github.com/JtotheThree/godot_curve_editor) - Copyright (c) 2024 JtotheThree, licensed under the [MIT license](https://github.com/JtotheThree/godot_curve_editor/blob/main/LICENSE)
--- scripts/curve_editor.gd
+++ scripts/curve_editor.gd
@@ -1,6 +1,6 @@
-# Modified from https://github.com/JtotheThree/godot_curve_editor/blob/main/curve_editor.gd
+# Adapted from https://github.com/JtotheThree/godot_curve_editor/blob/main/curve_editor.gd
 
-# --------------------------------------- #
+# ------------------------------------------------------------- #
 # MIT License
 #
 # Copyright (c) 2024 JtotheThree
@@ -22,7 +22,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-# --------------------------------------- #
+# ------------------------------------------------------------- #
 
 @tool
 class_name CurveEditor
