18c7171fd4e0d952158f57f2acc00f7a8a93c746
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-03-14 15:01:57 -0400

    Created vector2 controls

---
+++ scripts/vector2_controls.gd
@@ -0,0 +1,25 @@
+@tool
+extends VBoxContainer
+
+
+@onready var x_spin_box := $'XHBoxContainer/SpinBox'
+@onready var y_spin_box := $'YHBoxContainer/SpinBox'
+
+@export var value := Vector2(0, 0):
+	set(new_value):
+		if x_spin_box: x_spin_box.value = new_value.x
+		if y_spin_box: y_spin_box.value = new_value.y
+
+		value = new_value
+
+
+func _ready() -> void:
+	x_spin_box.value = value.x
+	y_spin_box.value = value.y
+
+
+func _on_x_spin_box_value_changed(new_value: float) -> void:
+	value.x = new_value
+
+func _on_y_spin_box_value_changed(new_value: float) -> void:
+	value.y = new_value
