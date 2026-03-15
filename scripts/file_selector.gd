45dec4af7b7876c603f928c00d574cb6736d76ef
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-03-15 10:04:40 -0400

    Updated file selector

--- scripts/file_selector.gd
+++ scripts/file_selector.gd
@@ -1,21 +1,26 @@
+@tool
 extends Control
 
-
-@export var file_mode := FileDialog.FileMode.FILE_MODE_SAVE_FILE
-@export var filters: PackedStringArray
 
 @onready var file_dialog_reference := $'FileDialog'
 @onready var line_edit_reference := $'LineEdit'
 
+@export var file_mode := FileDialog.FileMode.FILE_MODE_SAVE_FILE:
+	set(new_value):
+		if file_dialog_reference: file_dialog_reference.file_mode = new_value
+		file_mode = new_value
+
+@export var filters: PackedStringArray:
+	set(new_value):
+		if file_dialog_reference: file_dialog_reference.filters = new_value
+		filters = new_value
+
 var selected_path := ''
 
 
-func update_file_mode():
+func _ready() -> void:
 	file_dialog_reference.file_mode = file_mode
 	file_dialog_reference.filters = filters
-
-func _ready() -> void:
-	update_file_mode()
 
 
 func _on_select_button_pressed() -> void:
