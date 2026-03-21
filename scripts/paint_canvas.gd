2287fcf9ed6fb5a8a7f6d8305fe05fb737302774
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-03-21 17:24:39 -0400

    Connected stroke movement

--- scripts/paint_canvas.gd
+++ scripts/paint_canvas.gd
@@ -18,6 +18,9 @@ var canvas_image: Image
 
 var is_painting := false
 
+var brush_strokes := []
+var stroke_connections := []
+
 
 func _ready() -> void:
 	canvas_image = Image.create_empty(canvas_size.x, canvas_size.y, false, Image.FORMAT_RGBA8)
@@ -29,8 +32,13 @@ func _ready() -> void:
 
 	add_child(canvas_texture_rect_reference)
 
+
 func handle_mouse_button(event: InputEvent) -> void:
 	if !event.pressed:
+		if stroke_connections:
+			brush_strokes.append(stroke_connections)
+			stroke_connections.clear()
+
 		is_painting = false
 		return
 
@@ -40,7 +48,13 @@ func handle_mouse_button(event: InputEvent) -> void:
 func handle_mouse_motion(event: InputEvent) -> void:
 	if !is_painting: return
 
-	canvas_image.fill_rect(Rect2i(event.position.x, event.position.y, 10, 10), Color.BLUE)
+	if stroke_connections:
+		create_line(stroke_connections[-1], event.position)
+	else:
+		canvas_image.fill_rect(Rect2i(event.position.x, event.position.y, 10, 10), Color.BLUE)
+
+	stroke_connections.append(event.position)
+
 	canvas_texture_rect_reference.texture.update(canvas_image)
 
 func _input(event: InputEvent) -> void:
@@ -51,3 +65,11 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		handle_mouse_motion(event)
 		return
+
+
+func create_line(start_position: Vector2, end_position: Vector2) -> void:
+	var line_position := start_position
+
+	while line_position != end_position:
+		canvas_image.fill_rect(Rect2i(line_position.x, line_position.y, 10, 10), Color.BLUE)
+		line_position = line_position.move_toward(end_position, 1.0)
