d98d433c32562bd7d63cccb03ea31ca23d69043e
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-03-21 14:14:14 -0400

    Combined components.json into extension.cfg

--- scripts/tools/dynamic/extension_loader.gd
+++ scripts/tools/dynamic/extension_loader.gd
@@ -31,13 +31,8 @@ func load_unpacked_extension(path: String) -> void:
 		'description': config_file.get_value('extension', 'description')
 	}
 
-	var components_list_path = config_file.get_value('requirements', 'components')
-	if components_list_path != null:
-		components_list_path = path.path_join(components_list_path)
-
-		var components_list_file := FileAccess.open(components_list_path, FileAccess.READ)
-		var components_list: Array = JSON.parse_string(components_list_file.get_as_text())
-
+	var components_list = config_file.get_value('requirements', 'components')
+	if components_list != null:
 		for script in components_list:
 			var script_path := path.path_join(script)
 			var loaded_script = load(script_path)
@@ -68,21 +63,20 @@ func load_packed_extension(path: String) -> void:
 		'description': config_file.get_value('extension', 'description')
 	}
 
-	var components_list_path: String = config_file.get_value('requirements', 'components')
-	var components_list: Array = JSON.parse_string(zip_reader.read_file(components_list_path).get_string_from_utf8())
+	var components_list = config_file.get_value('requirements', 'components')
+	if components_list != null:
+		for script in components_list:
+			var temp_script_file := FileAccess.create_temp(FileAccess.WRITE, '', 'gd')
+			temp_script_file.store_buffer(zip_reader.read_file(script))
+			temp_script_file.close()
 
-	for script in components_list:
-		var temp_script_file := FileAccess.create_temp(FileAccess.WRITE, '', 'gd')
-		temp_script_file.store_buffer(zip_reader.read_file(script))
-		temp_script_file.close()
+			var script_path := temp_script_file.get_path()
+			var loaded_script = load(script_path)
 
-		var script_path := temp_script_file.get_path()
-		var loaded_script = load(script_path)
+			var component_id: String = script.get_file().get_basename()
 
-		var component_id: String = script.get_file().get_basename()
-
-		components[component_id] = {
-			'name': loaded_script.component_name,
-			'description': loaded_script.component_description,
-			'script': loaded_script
-		}
+			components[component_id] = {
+				'name': loaded_script.component_name,
+				'description': loaded_script.component_description,
+				'script': loaded_script
+			}
