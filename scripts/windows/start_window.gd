19c3b8e4728844104c2d5839690a62f7bb349a79
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-06-18 12:38:09 -0400

    Added confirmation to exit

--- scripts/windows/start_window.gd
+++ scripts/windows/start_window.gd
@@ -2,11 +2,14 @@ extends Control
 
 
 @onready var projects_list_reference := $'PanelContainer/VBoxContainer/VBoxContainer/ProjectsList'
+@onready var exit_confirmation_reference := $'ExitConfirmationDialog'
 
 var project_list: Array[String] = []
 
 
 func _ready() -> void:
+	get_tree().set_auto_accept_quit(false)
+
 	var parent := get_parent()
 	if parent is Window:
 		parent.title = ProjectSettings.get_setting('application/config/name') + ' - Project Manager'
@@ -18,8 +21,18 @@ func _ready() -> void:
 		project_list.append(config.get_value('projects', project_name))
 		projects_list_reference.add_item(project_name)
 
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		exit_confirmation_reference.popup_centered()
+
 func _on_new_button_pressed() -> void:
 	ProjectLoader.create_new_project_new_window()
 
 func _on_projects_list_item_activated(index: int) -> void:
 	ProjectLoader.load_project_new_window(project_list[index])
+
+func _on_exit_confirmation_dialog_canceled() -> void:
+	exit_confirmation_reference.hide()
+
+func _on_exit_confirmation_dialog_confirmed() -> void:
+	get_tree().quit()
