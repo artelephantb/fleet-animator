869b402b6ce91bfcf646a074c85f7a9763d2e08c
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-06-24 16:34:20 -0400

    Added property functions to GraphComponent

--- base_extensions/core/components/main.gd
+++ base_extensions/core/components/main.gd
@@ -9,11 +9,19 @@ static var component_ids: PackedStringArray = [
 ]
 
 static var component_names := {
-	'recieve_signal': 'Recieve Signal',
+	'recieve_signal': 'On Receiving Signal',
 	'change_position': 'Change Position',
 	'change_scale': 'Change Scale',
 	'change_rotation': 'Change Rotation',
 	'wait_frames': 'Wait Frames'
+}
+
+var component_looks := {
+	'recieve_signal': looks_recieve_signal,
+	'change_position': looks_change_position,
+	'change_scale': looks_change_scale,
+	'change_rotation': looks_change_rotation,
+	'wait_frames': looks_wait_frames
 }
 
 var component_bindings := {
@@ -25,8 +33,22 @@ var component_bindings := {
 }
 
 
-func _init() -> void:
-	pass
+func looks_recieve_signal(component: GraphComponent):
+	component.title = 'On Receiving Signal'
+
+func looks_change_position(component: GraphComponent):
+	component.title = 'Change Position'
+
+func looks_change_scale(component: GraphComponent):
+	component.title = 'Change Scale'
+
+func looks_change_rotation(component: GraphComponent):
+	component.title = 'Change Rotation'
+
+func looks_wait_frames(component: GraphComponent):
+	component.title = 'Wait Frames'
+	component.add_label('Across Frames')
+	component.add_float_property('AcrossFrames', 0.0)
 
 
 func recieve_signal():
--- scripts/graph_component.gd
+++ scripts/graph_component.gd
@@ -10,9 +10,29 @@ func _init(catagory: StringName, type: StringName) -> void:
 	self.catagory = catagory
 	self.type = type
 
+	AnimationEngine.component_catagories[catagory].component_looks[type].call(self)
+
 
 func set_inputs(inputs: Dictionary) -> void:
 	pass
 
 func get_inputs() -> Dictionary:
 	return {}
+
+
+func add_label(text: String) -> void:
+	var label := Label.new()
+	label.text = text
+	add_child(label)
+
+func add_float_property(id: StringName, default := 0.0) -> void:
+	var spin_box := SpinBox.new()
+
+	spin_box.value = default
+	spin_box.min_value = -10000.0
+	spin_box.max_value = 10000.0
+	spin_box.step = 0.01
+	spin_box.allow_greater = true
+	spin_box.allow_lesser = true
+
+	add_child(spin_box)
